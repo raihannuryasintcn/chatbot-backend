a682137e3a2534fc10701de4cd1731258d2a6817
raihannuryasintcn <raihannuryasintcn@gmail.com>
2025-01-10 18:17:12 +0700

    debugging railway port

--- app.py
+++ app.py
@@ -192,8 +192,6 @@ def health_check():
         "model_name": chatbot.model.get_sentence_embedding_dimension()
     })
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
 port = int(os.environ.get("PORT", 8080))
-app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port)
