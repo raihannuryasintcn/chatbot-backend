ec701b9612d64c858ec8f600011a6ef92e98b097
raihannuryasintcn <raihannuryasintcn@gmail.com>
2025-01-10 13:49:34 +0700

    configure port for railway

--- app.py
+++ app.py
@@ -195,5 +195,5 @@ def health_check():
 if __name__ == '__main__':
     app.run(debug=True)
 
-port = int(os.environ.get("PORT", 5000))
+port = int(os.environ.get("PORT", 8080))
 app.run(host='0.0.0.0', port=port)
